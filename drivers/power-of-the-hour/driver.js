dd92ff1c3f46eadebb6bb07828115c61ceb91a4e
Kjetil Haugland <hauglandkjetil@gmail.com>
2020-10-07 20:36:45 +0200

    Power of the hour in norwegian as well. Possible to add multiple devices

--- drivers/power-of-the-hour/driver.js
+++ drivers/power-of-the-hour/driver.js
@@ -4,26 +4,28 @@ const Homey = require('homey');
 const calculations = require('../../lib/calculations');
 
 class PowerOfTheHourDriver extends Homey.Driver {
-  /**
-   * onInit is called when the driver is initialized.
-   */
+
   async onInit() {
-    this.devices = [{
-      name: 'Power of the hour',
-      data: {
-        id: calculations.uuid(),
-      }
-    }]
     this.log('Power of the hour driver has been initialized');
   }
 
   async onPairListDevices() {
-    return this.devices;
+    return [{
+      name: 'Power of the hour',
+      data: {
+        id: calculations.uuid(),
+      }
+    }];
   }
 
   async onPair(socket) {
     socket.on("list_devices", () => {
-      socket.emit("list_devices", this.devices);
+      socket.emit("list_devices", [{
+        name: 'Power of the hour',
+        data: {
+          id: calculations.uuid(),
+        }
+      }]);
     });
   }
 
