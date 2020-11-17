f7ed68233cb3ecb12af3243a41b1b30ed142e6af
Kjetil Haugland <kjetil.haugland@vard.com>
2020-11-17 07:49:42 +0100

    Added a few more tests

--- test/calculations.test.js
+++ test/calculations.test.js
@@ -71,16 +71,17 @@ test('Remaining hour 12:45 to equal 0.25', () => {
 });
 
 // isNewHour
+// same day
 test('Is new hour between 12:00 and 12:30 the same day to equal false', () => {
     const oldest = '2020-10-21T12:00:00.000Z'
     const newest = '2020-10-21T12:30:00.000Z'
     expect(calculations.isNewHour(newest,oldest)).toBe(false);
 })
 
-test('Is new hour between 12:00 and 12:30 the next day to equal true', () => {
-    const oldest = '2020-10-21T12:00:00.000Z'
-    const newest = '2020-10-22T12:30:00.000Z'
-    expect(calculations.isNewHour(newest,oldest)).toBe(true);
+test('Is new hour between 12:15 and 12:20 the same day to equal false', () => {
+    const oldest = '2020-10-21T12:15:00.000Z'
+    const newest = '2020-10-21T12:20:00.000Z'
+    expect(calculations.isNewHour(newest,oldest)).toBe(false);
 })
 
 test('Is new hour between 12:00 and 13:00 the same day to equal true', () => {
@@ -89,13 +90,43 @@ test('Is new hour between 12:00 and 13:00 the same day to equal true', () => {
     expect(calculations.isNewHour(newest,oldest)).toBe(true);
 })
 
-test('Is new hour between 12:00 and 13:00 the next day to equal true', () => {
+test('Is new hour between 12:15 and 13:20 the same day to equal true', () => {
+    const oldest = '2020-10-21T12:15:00.000Z'
+    const newest = '2020-10-21T13:20:00.000Z'
+    expect(calculations.isNewHour(newest,oldest)).toBe(true);
+})
+// next day
+test('Is new hour between 12:00 and 12:30 the next day to equal true', () => {
     const oldest = '2020-10-21T12:00:00.000Z'
-    const newest = '2020-10-22T13:00:00.000Z'
+    const newest = '2020-10-22T12:30:00.000Z'
     expect(calculations.isNewHour(newest,oldest)).toBe(true);
 })
 
-test('Prediction: 1 minute history, 1 reading, 1 valid,  at 12:00 to equal 1000W', () => {
+test('Is new hour between 12:15 and 12:20 the next day to equal true', () => {
+    const oldest = '2020-10-21T12:15:00.000Z'
+    const newest = '2020-10-22T12:20:00.000Z'
+    expect(calculations.isNewHour(newest,oldest)).toBe(true);
+})
+
+test('Is new hour between 12:00 and 11:00 the next day to equal true', () => {
+    const oldest = '2020-10-21T12:00:00.000Z'
+    const newest = '2020-10-22T11:00:00.000Z'
+    expect(calculations.isNewHour(newest,oldest)).toBe(true);
+})
+
+test('Is new hour between 12:10 and 11:10 the next day to equal true', () => {
+    const oldest = '2020-10-21T12:10:00.000Z'
+    const newest = '2020-10-22T11:10:00.000Z'
+    expect(calculations.isNewHour(newest,oldest)).toBe(true);
+})
+
+test('Is new hour between 12:15 and 13:10 the next day to equal true', () => {
+    const oldest = '2020-10-21T12:15:00.000Z'
+    const newest = '2020-10-22T13:10:00.000Z'
+    expect(calculations.isNewHour(newest,oldest)).toBe(true);
+})
+
+test('Prediction: 1 minute history, 1 reading, 1 valid, at 12:00 to equal 1000W', () => {
     const readings = [
         {consumption: 1000, timestamp: '2020-10-21T12:00:00.000Z'},
     ]
