778b753e5d472e86670a18e761002479de82cc67
Kjetil Haugland <hauglandkjetil@gmail.com>
2021-10-16 17:35:17 +0200

    Initial version

---
+++ drivers/power-of-the-hour/driver.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const  { v4 : uuid} = require('uuid');
+const Homey = require('homey');
+const calculations = require('../../lib/calculations');
+
+class PowerOfTheHourDriver extends Homey.Driver {
+
+  async onInit() {
+    this.log('Power of the hour driver has been initialized');
+  }
+
+  async onPairListDevices() {
+    return [{
+      name: 'Power of the hour',
+      data: {
+        id: uuid(),
+      }
+    }];
+  }
+
+  async onPair(session) {
+    session.setHandler("list_devices", async () => {
+      return {
+        name: 'Power of the hour',
+        data: {
+          id: uuid(),
+        }
+      };
+    });
+  }
+
+}
+
+module.exports = PowerOfTheHourDriver;
