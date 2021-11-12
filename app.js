1c88f8b5fe67b305ab50533b8e0737e190877761
Kjetil Haugland <kjetil.haugland@vard.com>
2021-11-12 10:08:51 +0100

    Removed usage of store since it was blocking. Refresh every 60 seconds to keep notifications alive. Refactored settings

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const Homey = require('homey');
 
 module.exports = class PowerOfTheHour extends Homey.App {
   async onInit() {
-    this.log('Power of the hour is running...');
-
-    this.homey.flow.getActionCard('consumption_updated')
-      .registerRunListener((args, state) => { args.device.onActionConsumptionChanged(args, state) });
-
-    this.log('Done initializing flows...');
+    this.log('Power of the hour is running...');  
   }
 }
--- drivers/power-of-the-hour/driver.js
+++ drivers/power-of-the-hour/driver.js
@@ -7,7 +7,11 @@ const calculations = require('../../lib/calculations');
 class PowerOfTheHourDriver extends Homey.Driver {
 
   async onInit() {
+    this.homey.flow.getActionCard('consumption_updated')
+      .registerRunListener((args, state) => { args.device.onActionConsumptionChanged(args, state) });
+      
     this.log('Power of the hour driver has been initialized');
+
   }
 
   async onPairListDevices() {
