3fb0965cec13b0580d779691a8864b66af3fc4e4
Kjetil Haugland <hauglandkjetil@gmail.com>
2023-03-28 20:05:15 +0200

    Fixed issue where pair view would not list any device on Homey Pro (Early 2023) - Closes #82

--- drivers/power-of-the-hour/driver.js
+++ drivers/power-of-the-hour/driver.js
@@ -1,34 +1,23 @@
 'use strict';
 
-const { v4 : uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 const Homey = require('homey');
 
 class PowerOfTheHourDriver extends Homey.Driver {
-
   async onInit() {
     this.log('Power of the hour driver has been initialized');
   }
 
   async onPairListDevices() {
-    return [{
-      name: 'Power of the hour',
-      data: {
-        id: uuid(),
-      },
-    }];
-  }
-
-  async onPair(session) {
-    session.setHandler('list_devices', async () => {
-      return {
+    return [
+      {
         name: 'Power of the hour',
         data: {
           id: uuid(),
         },
-      };
-    });
+      },
+    ];
   }
-
 }
 
 module.exports = PowerOfTheHourDriver;
